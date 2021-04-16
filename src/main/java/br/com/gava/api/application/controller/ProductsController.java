cd7e19445197c54eaca0f75a34090877dc5a7540
Ernandes <endsalone@yahoo.com.br>
2021-04-15 22:22:46 -0300

    chore: HAL links as abstract class

--- src/main/java/br/com/gava/api/application/controller/ProductsController.java
+++ src/main/java/br/com/gava/api/application/controller/ProductsController.java
@@ -6,28 +6,20 @@ import br.com.gava.api.common.Pagination;
 import br.com.gava.api.domain.entities.Products;
 import br.com.gava.api.domain.products.IProductsRepository;
 import br.com.gava.api.domain.products.ProductsDto;
-import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-
 @RestController
 @RequestMapping(value = "/products", produces = "application/hal+json")
-public class ProductsController extends HalList {
+public class ProductsController extends HalList<ProductsDto> {
 
   private final IProductsRepository service;
-  private final ModelMapper modelMapper = new ModelMapper();
 
   ProductsController(ProductsService service) {
     this.service = service;
@@ -44,36 +36,15 @@ public class ProductsController extends HalList {
       : this.service.findByNameOrDescription(search, pagination);
 
     this.setPageItems(productsPage);
+    this.setDto(ProductsDto.class);
+    CollectionModel<EntityModel<ProductsDto>> response = this.makeLinks(pagination, search);
 
-    List<ProductsDto> productsDto = productsPage
-      .getContent()
-      .stream()
-      .map(product -> modelMapper.map(product, ProductsDto.class))
-      .collect(Collectors.toList());
-
-    List<EntityModel<ProductsDto>> entityProducts = productsDto
-      .stream()
-      .map(product -> EntityModel
-        .of(product)
-        .add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getProduct(product.getId())).withSelfRel())
-      )
-      .collect(Collectors.toList());
-
-    Link selfUrl = WebMvcLinkBuilder
-      .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).listProducts(pagination, search))
-      .slash(pagination.toString())
-      .withSelfRel();
-
-
-    CollectionModel<EntityModel<ProductsDto>> response = CollectionModel
-      .of(entityProducts)
-      .add((selfUrl));
     return ResponseEntity.status(200).body(response);
   }
 
   @Override
   public ResponseEntity<?> getItem(@PathVariable long id) {
-    return ResponseEntity.status(200).body(id);
+    return ResponseEntity.status(200).body("id");
   }
 
 }
--- src/main/java/br/com/gava/api/common/HalList.java
+++ src/main/java/br/com/gava/api/common/HalList.java
@@ -1,9 +1,12 @@
 package br.com.gava.api.common;
 
 import br.com.gava.api.domain.entities.IEntity;
+import lombok.Setter;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +17,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
-public abstract class HalList<T>  {
+public abstract class HalList<T extends IEntity>  {
 
+  @Setter
   private Page<?> pageItems;
-  private T dto;
+  @Setter
+  private Class<T> dto;
 
   private final ModelMapper modelMapper = new ModelMapper();
 
@@ -30,27 +35,29 @@ public abstract class HalList<T>  {
   @GetMapping("/{id}")
   public abstract ResponseEntity<?> getItem(@PathVariable long id);
 
-  protected void setPageItems(Page<?> pageItems) {
-    this.pageItems = pageItems;
-  }
-
-  protected void setDto(T dto) {
-    this.dto = dto;
-  }
-
-  private void makeLinks(Class dto) {
-    List<?> listDto = this.pageItems
+  protected CollectionModel<EntityModel<T>> makeLinks(Pagination pagination, String search) {
+    List<T> listDto = this.pageItems
       .getContent()
       .stream()
-      .map(item -> modelMapper.map(item, this.dto.getClass()))
+      .map(item -> modelMapper.map(item, this.dto))
       .collect(Collectors.toList());
 
-//    List<EntityModel<?>> entityModel = listDto
-//      .stream()
-//      .map(item -> EntityModel
-//        .of(item)
-//        .add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getItem(item.getId())).withSelfRel())
-//      )
-//      .collect(Collectors.toList());
+    List<EntityModel<T>> entityModel = listDto
+      .stream()
+      .map(item -> EntityModel
+        .of(item)
+        .add()
+        .add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getItem(item.getId())).withSelfRel())
+      )
+      .collect(Collectors.toList());
+
+    Link selfUrl = WebMvcLinkBuilder
+      .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAll(pagination, search))
+      .slash(pagination.toString())
+      .withSelfRel();
+
+    return CollectionModel
+      .of(entityModel)
+      .add((selfUrl));
   }
 }
